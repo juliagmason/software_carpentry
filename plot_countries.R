83a401f717801d6c6913cd629ab908d5d997decd
juliagmason <jgmason@stanford.edu>
2017-12-01 13:21:35 -0800

    more functions and package development

--- plot_countries.R
+++ plot_countries.R
@@ -80,19 +80,47 @@ is_developed <- function (cntry, threshold = 12000) {
   
 }
 
+is_developed ("United States")
+is_developed ("United States", threshold = 5000000000)
+
 # one way to specify a folder to save things in a function is to put folder = "." in your function terms. 
 # country_plot <- function (cntry, folder = ".")
 # then can specify specific folder names in your function call. 
 # country_plot ("Peru", "developing")
 
-is_developed ("United States")
-is_developed ("United States", threshold = 5000000000)
+# or, modify png name within function. 
 
+# rework plot_country function for flexible filename
+plot_country <- function(cntry, png){
+  # debug?
+  # browser()
+  
+  # tell us what country you're doing. 
+  cat ("country_plot(", cntry, ")\n") # cat pastes and prints. \n means new line
+  
+  g <- gapminder %>%
+    filter(country == cntry) %>%
+    ggplot(aes(x = year, y = gdpPercap)) +
+    geom_point() +
+    geom_smooth() +
+    labs(title = cntry)
+  
+  ggsave(filename = png, plot = g)
+}
+
+
+# loop over countries, and save in appropriate folders
 for (k in countries){
   if (is_developed(k)) {
     png <- paste0 ("developed/gdp_", k, ".png")
     } else{
     png <- paste0 ("developing/gdp_", k, ".png")
     }
+
+    plot_country (k, png)
 }
-                   
+
+# it's easy to make your own package. make a new folder with the package name
+# cursor inside function: code --> insert Roxygen skeleton. Fill in parameters, name, etc descriptions
+library (devtools)
+
